f9a7bc253cf5c82464521f336359f56ef8fd2e73
Waleed Mohamed <solimanwaleed@rocketmail.com>
2026-07-12 19:47:09 +0300

    Fix cafeteria checkout and support multiple daily slots

--- academies/tests.py
+++ academies/tests.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 from django.test import TestCase, override_settings
 from django.urls import reverse
 
-from .constants import OPERATION_PLACE_CHOICES, TIME_CHOICES
+from .constants import OPERATION_PLACE_CHOICES, TIME_CHOICES, WEEKDAY_AR
+from .forms import AcademyForm
+from .views import _academy_schedule_occurrences_for_date, _calculate_variable_income_by_facility
 from .models import (
     Academy,
     CafeteriaCategory,
@@ -73,6 +75,29 @@ class ApplicationFlowsTests(TestCase):
         self.assertContains(response, '<td>8</td>', html=True)
         self.assertContains(response, '<td>3</td>', html=True)
         self.assertContains(response, '<td class="fw-bold">7</td>', html=True)
+
+    def test_variable_academy_accepts_multiple_intervals_in_same_day(self):
+        place = OPERATION_PLACE_CHOICES[0][0]
+        selected_date = date.today()
+        day_name = WEEKDAY_AR[selected_date.weekday()]
+        schedule = [
+            {'place': place, 'day': day_name, 'start_time': TIME_CHOICES[0][0], 'end_time': TIME_CHOICES[2][0], 'hourly_rent': 100},
+            {'place': place, 'day': day_name, 'start_time': TIME_CHOICES[4][0], 'end_time': TIME_CHOICES[6][0], 'hourly_rent': 200},
+        ]
+        form = AcademyForm(data={
+            'name': 'أكاديمية مواعيد متعددة', 'sport_activity': 'كرة قدم',
+            'company_name': 'شركة اختبار', 'manager_name': 'مدير اختبار',
+            'manager_phone': '01000000001', 'contract_start_date': selected_date.isoformat(),
+            'contract_end_date': selected_date.isoformat(), 'subscription_type': 'variable',
+            'monthly_subscription': 0, 'variable_rent_type': 'hour', 'variable_rent_value': 0,
+            'eess_share_percentage': 0, 'security_deposit': 0,
+            'training_schedule_data': json.dumps(schedule), 'operation_place': [place],
+        })
+        self.assertTrue(form.is_valid(), form.errors.as_json())
+        academy = form.save()
+        occurrences = _academy_schedule_occurrences_for_date(academy, selected_date)
+        self.assertEqual(len(occurrences), 4)
+        self.assertEqual(_calculate_variable_income_by_facility(academy, selected_date.year, selected_date.month), 300)
 
     def test_daily_booking_checkout_from_operation(self):
         booking = DailyBooking.objects.create(
